6186fc618204ce0ce51fdc189b326feb57b9affa
Zach Bogart <31786233+zachbogart@users.noreply.github.com>
2025-02-04 05:40:02 +0000

    smart note test

--- smartnotes/urls.py
+++ smartnotes/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-
-from home import views
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("home", views.home)
+    path("", include('home.urls'))
 ]
