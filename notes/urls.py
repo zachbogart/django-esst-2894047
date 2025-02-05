457f79cd4e3ab48a404b7d8f6bb6e8c7d5f60523
Zach Bogart <31786233+zachbogart@users.noreply.github.com>
2025-02-05 03:27:49 +0000

    bootstrap adding

--- notes/urls.py
+++ notes/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.NotesListView.as_view()),
-    path('notes/<int:pk>', views.NotesDetailView.as_view()),
+    path('notes', views.NotesListView.as_view(), name="notes.list"),
+    path('notes/<int:pk>', views.NotesDetailView.as_view(), name="notes.detail"),
     path('popular', views.PopularNotesListView.as_view())
 ]
